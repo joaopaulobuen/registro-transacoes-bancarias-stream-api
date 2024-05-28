ab7c592674d1a93889209c580cb4f53565d87baf
joaopaulobuen <joaopaulobuen@gmail.com>
2024-05-28 16:16:21 -0300

    commit inicial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+        int quantidadeTransacoes = scanner.nextInt();
+
+        List<Transacao> transacoes = new ArrayList<>();
+
+        for (int i = 1; i <= quantidadeTransacoes; i++) {
+
+            char tipoTransacao = scanner.next().charAt(0);
+            double valorTransacao = scanner.nextDouble();
+
+            // TODO: Criar uma nova transação e adicioná-la à lista de transações
+            Transacao transacao = new Transacao(tipoTransacao, valorTransacao);
+            transacoes.add(transacao);
+
+            // Verifica e atualiza o saldo da conta com base no tipo de transação
+            if (Character.toUpperCase(transacao.getTipo()) == 'D') {
+                saldo += valorTransacao;
+            } else if (Character.toUpperCase(transacao.getTipo()) == 'S') {
+                saldo -= valorTransacao;
+            }
+        }
+        // Usar Locale.US para garantir que o ponto seja usado como separador decimal
+        Locale locale = Locale.US;
+
+        System.out.printf(String.format(locale,"\nSaldo : %.1f", saldo));
+        System.out.println("\nTransacoes:");
+        transacoes.stream()
+                .map(transacao -> String.format(locale,"%c %s %.1f",transacao.getTipo(),"de",transacao.getValor()))
+                .toList()
+                .forEach(System.out::println);
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
+
+class Transacao {
+    private char tipo;
+    private double valor;
+
+    public Transacao(char tipo, double valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
